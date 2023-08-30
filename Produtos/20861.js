11dd5ecf071d4436971ac985d3074f29a89d2c06
eduardo.rezzadori <eduardo.rezzadori@inova.ind.br>
2023-08-30 15:49:52 -0300

    Initial commit

---
+++ Produtos/20861.js
@@ -0,0 +1,86 @@
+class TestScript {
+    constructor(eventMap, event) {
+
+        this.EventMap = eventMap
+        this.Event = event
+
+        //instancias de modulos
+        this.Std = new SmartTestDevice(1, 3, "dc4", "dc3", "dc2", "dc1")
+        this.TestReport = new RelatorioTeste()
+
+        this.RelaysState = []
+
+        this.TestComponents = {
+            Base: {
+                Name: "BS-80",
+                Image: "Imagens\\BS-80.png",
+            },
+            Fixture: {
+                Name: "FX-72",
+                Image: "Imagens\\padrao.png",
+            },
+        }
+
+        //roteiro de teste
+        this.Run()
+            .then(async (noTracking) => {
+                this.Std.MoveUp()
+                this.Controller.Processing = false
+                this.VoltageReader.Processing = false
+                await SerialPVIUtil.closeAllPorts()
+                GeneralCompose.ShutDown()
+                console.timeEnd("Run")
+
+                if (noTracking) {
+                    UI.displayReport(this.TestReport)
+                } else {
+                    await UI.setMsg("Finalizando rastreamento...")
+                    RelatorioTeste.OrdenaRelatorio(this.TestReport)
+                    this.Rast.setReport(this.TestReport)
+                    const rastEndSucess = await this.Rast.end(RastUtil.evalReport(this.TestReport))
+                    if (!rastEndSucess) { this.TestReport.AddTesteFuncional("Rastreamento End", this.Rast.EndInfo.Message, -1, false); throw new Error("Impossível finalizar o rastreamento") }
+                    UI.displayReport(this.TestReport)
+                }
+            })
+            .catch(async (error) => {
+                this.Std.MoveUp()
+                console.timeEnd("Run")
+                this.TestReport.AddTesteFuncional("Exception", error, -1, false)
+                UI.displayReport(this.TestReport)
+            })
+    }
+
+    async Run() {
+        console.time("Run")
+
+        //#region Infra Setup
+        if (RastUtil.isFirstExec()) {
+            await UI.modalInfo(this.TestComponents.Base.Image, `Utlize a base ${this.TestComponents.Base.Name}`)
+            await UI.modalInfo(this.TestComponents.Fixture.Image, `Utlize o fixture ${this.TestComponents.Fixture.Name}`)
+            await UI.modalInfo("Imagens/setupTrafoUSB.jpeg", "Conecte o trafo e o USB na base conforme indicado")
+            await UI.modalInfo("Imagens/gravador.jpg", "Conecte o gravador à tomada, ao USB e à base, utilizando o isolador, conforme indicado")
+        }
+
+        this.Std.EmergencyObserver().then((info) => { console.error(info); alert(info.msg); return true })
+        const moveUpInit = await this.Std.MoveUp()
+        if (!moveUpInit.result) { this.TestReport.AddTesteFuncional("Sobe Motor [Inicio]", moveUpInit.msg, -1, false); return true }
+        //#region Infra Setup
+
+        //#region Rast
+        await UI.setMsg("Iniciando rastreamento...")
+        RastUtil.setValidations(RastUtil.ENABLED, RastUtil.ENABLED, RastUtil.ENABLED, RastUtil.DISABLED)
+        this.Rast = new RastPVI(this.EventMap, this.Event, this.TestComponents) //BS-80.2/REV2   FX-48.1/REV1
+        await RastUtil.setOperador()
+        await this.Rast.setSerialNumber() //1000008256844
+
+        const rastInitSucess = await this.Rast.init()
+        if (!rastInitSucess) { this.TestReport.AddTesteFuncional("Rastreamento Init", this.Rast.InitInfo.Message, -1, false); return }
+        UI.setTitle(this.Rast.InitInfo.item.OpInfo.Product.Name)
+        const finalFirmwarePath = this.Rast.InitInfo.item.OpInfo.OpProcesses.find(process => process.ID == "TF").Firmware
+        //#endregion Rast
+
+        //#region Start
+
+        //#endregion
+    }
+}
---
+++ libs/UI.js
@@ -0,0 +1,205 @@
+class UI {
+
+    static async init() {
+        this.onRestartClick()
+        return await this.hideAll()
+    }
+
+    static reload() {
+        pvi.runInstructionS("RESET", [])
+        location.reload()
+    }
+
+    static async setImage(imagem) {
+        html_imgTest.src = imagem
+        html_imgTest.style.display = "initial"
+        return await this.waitToRender()
+    }
+
+    static async closeImage() {
+        html_imgTest.style.display = "none"
+        return await this.waitToRender()
+    }
+
+    static async setMsg(msg) {
+        html_testMsg.innerText = msg
+        return await this.waitToRender()
+    }
+
+    static async setTitle(msg) {
+        html_testTitle.innerText = msg
+        return await this.waitToRender()
+    }
+
+    static async hideAll() {
+        html_btnAvanca.style.visibility = "hidden"
+        html_btnAux1.style.visibility = "hidden"
+        html_btnAux2.style.visibility = "hidden"
+        html_btnAux3.style.visibility = "hidden"
+        html_btnNao.style.visibility = "hidden"
+        html_btnSim.style.visibility = "hidden"
+        html_imgTest.style.display = "none"
+        html_modal.style.display = "none"
+        return await this.waitToRender()
+    }
+
+    static async onKeyDown(key, toReturnValue) {
+        return new Promise((resolve) => {
+            document.addEventListener('keydown', (event) => {
+                if (event.key == key) {
+                    resolve(toReturnValue)
+                }
+            })
+        })
+    }
+
+    static async onClick(element, toReturnValue) {
+        return new Promise((resolve) => {
+            element.addEventListener('click', () => {
+                resolve(toReturnValue)
+            }, { once: true })
+        })
+    }
+
+    static async onRestartClick() {
+        const onClick = await this.onClick(html_btnRestart, () => { this.reload() })
+        onClick()
+    }
+
+    static async interactionTimeout(timeout, toReturnValue) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(toReturnValue)
+            }, timeout)
+        })
+    }
+
+    static async waitToRender(delay = 10) {
+        return new Promise((resolver) => {
+            setTimeout(() => {
+                resolver()
+            }, delay)
+        })
+    }
+
+    static async modalInfo(image, message) {
+        return new Promise(async (resolve) => {
+
+            html_txtModal.disabled = true
+            html_modalImg.src = image
+            html_txtModal.value = message
+            html_txtModal.style.minHeight = "100px"
+            html_modal.style.display = ""
+            await this.waitToRender()
+
+            await this.onClick(html_btnOkModal)
+
+            html_modal.style.display = "none"
+            await this.waitToRender()
+            resolve()
+        })
+    }
+
+    static async yesNo(timeout = 15000) {
+
+        html_btnSim.style.visibility = "visible"
+        html_btnNao.style.visibility = "visible"
+
+        await this.waitToRender()
+
+        const timeOut = this.interactionTimeout(timeout, null)
+
+        const Up = this.onKeyDown("ArrowUp", true)
+        const KeyS = this.onKeyDown("s", true)
+        const Down = this.onKeyDown("ArrowDown", false)
+        const KeyN = this.onKeyDown("n", false)
+        const ClickS = this.onClick(html_btnSim, true)
+        const ClickN = this.onClick(html_btnNao, false)
+
+        const choice = await Promise.race([Up, Down, KeyS, KeyN, ClickS, ClickN, timeOut])
+
+        html_btnSim.style.visibility = "hidden"
+        html_btnNao.style.visibility = "hidden"
+
+        await this.waitToRender()
+
+        return choice
+    }
+
+    static async advance(timeout = 15000) {
+
+        html_btnAvanca.style.visibility = "visible"
+        await this.waitToRender()
+
+        const timeOut = this.interactionTimeout(timeout, false)
+        const Click = this.onClick(html_btnAvanca, true)
+        const Right = this.onKeyDown("ArrowRight", true)
+
+        const interacted = await Promise.race([Click, Right, timeOut])
+
+        html_btnAvanca.style.visibility = "hidden"
+        await this.waitToRender()
+
+        return interacted
+    }
+
+    static async displayReport(report) {
+
+        let falhas = new Map()
+
+        if (report.TesteFuncional.length != 0 || report.TesteComponentes.length != 0) {
+
+            report.TesteFuncional.forEach((teste) => {
+                if (!teste.Resultado) {
+                    falhas.set(teste.Nome, teste.Descricao)
+                }
+            })
+
+            report.TesteComponentes.forEach((teste) => {
+                if (!teste.Resultado) {
+                    falhas.set(teste.Designator, `Esperava: ${teste.Referencia} +- ${teste.Aceitacao}, Obteve: ${teste.Valor}`)
+                }
+            })
+        }
+
+        if (falhas.size == 0) {
+            await this.displayPass()
+            await this.interactionTimeout(3000)
+            this.reload()
+
+        } else {
+            let texto = ""
+
+            falhas.forEach((valor, chave) => {
+                texto += chave + " -> " + valor + "\n"
+            })
+            html_txtModal.value = "Falha nos testes:\n" + texto
+
+            await this.displayFail()
+            await this.onClick(html_btnOkModal)
+            this.reload()
+        }
+    }
+
+    static async displayPass() {
+        html_txtModal.disabled = true
+        html_modalTitle.style.fontSize = "30px"
+        html_modal.style.display = ""
+        html_modalTitle.innerText = "Aprovado"
+        html_modalTitle.style.color = "green"
+        html_modalImg.src = "images/OK.png"
+        html_txtModal.style.display = "none"
+        await this.waitToRender()
+    }
+
+    static async displayFail() {
+        html_txtModal.disabled = true
+        html_modalTitle.style.fontSize = "30px"
+        html_modal.style.display = ""
+        html_modalTitle.innerText = "Reprovado"
+        html_modalTitle.style.color = "red"
+        html_modalImg.src = "images/NOK.png"
+        html_txtModal.style.display = ""
+        await this.waitToRender()
+    }
+}
